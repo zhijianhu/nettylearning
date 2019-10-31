e2f7ecd032a37e36b86d3afaf6835d1258fbcf8b
zhijianhu <2810339711@qq.com>
2019-10-31 19:45:31 +0800

    '客户端服务器'

---
+++ src/main/java/the/flash/client/NettyClient.java
@@ -0,0 +1,9 @@
+package the.flash.client;
+
+/**
+ * @author huzhijian
+ * @version 1.0
+ * @Date 2019/10/31 16:48
+ */
+public class NettyClient {
+}
---
+++ src/main/java/the/flash/server/NettyServer.java
@@ -0,0 +1,9 @@
+package the.flash.server;
+
+/**
+ * @author huzhijian
+ * @version 1.0
+ * @Date 2019/10/31 15:43
+ */
+public class NettyServer {
+}
